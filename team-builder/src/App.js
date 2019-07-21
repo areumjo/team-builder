a2066b509deba079e97b0a6b178d33da6bbe6182
Areum Jo <areumjo1@gmail.com>
2019-07-21 19:07:22 -0400

    Imported useState hook and declared a default value

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Form from "./components/Form.js";
+
 import './App.css';
 
 function App() {
+  const [ member, setMember ] = useState({ name:"", nickname:"" });
+  const handleChange = event => {
+    setMember({...member, [event.target.name]: event.target.value})
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hi {member.name}
+      <Form />
     </div>
   );
 }
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Form() {
+    return(
+        <form>
+            <label>testing:
+               <input />
+            </label>
+        </form>
+    )
+}
+
+export default Form;
